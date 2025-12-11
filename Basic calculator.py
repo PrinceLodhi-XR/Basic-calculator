d0a5ee716165b627d9b674dab89a03ecd4fa623c
PrinceLodhi-XR <lodhiprince785@gmail.com>
2025-12-11 23:16:22 +0530

    Add files via upload

---
+++ Basic calculator.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number"))
+b = float(input("enter second number"))
+op = input("Enter operation(+ - * /): ")
+
+if op == "+": print(a+b)
+elif op == "-": print(a-b)
+elif op == "*": print(a*b)
+elif op == "/": print(a/b)
+else:print("Invalid")
